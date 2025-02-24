1994bf958942e23036f65dd13d69c8da456d1b79
Md Ammar <ammaratif559@gmail.com>
2025-02-24 18:16:45 +0000

    changed middleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,25 +1,25 @@
-const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const jwt = require("jsonwebtoken");
+const { User } = require("../models/User");
 
 const authMiddleware = async (req, res, next) => {
-  const token = req.header('Authorization').replace('Bearer ', '');
+  const token = req.header("Authorization").replace("Bearer ", "");
+
   if (!token) {
-    return res.status(401).json({ error: 'No token provided' });
+    return res.status(401).json({ error: "No token provided" });
   }
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    const user = await User.findOne({ _id: decoded._id, 'tokens.token': token });
+    const user = await User.findById(decoded._id);
 
     if (!user) {
-      throw new Error();
+      return res.status(404).json({ error: "User not found" });
     }
 
     req.user = user;
-    req.token = token;
     next();
   } catch (err) {
-    res.status(401).json({ error: 'Unauthorized' });
+    res.status(401).json({ error: "Invalid token" });
   }
 };
 
